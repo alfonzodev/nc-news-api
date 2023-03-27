c5ab23e7b0278680824e5104c7aaf9951fcb3f83
Jose Alfonzo <alfonzo.dev@gmail.com>
2023-03-27 15:10:16 +0100

    Solve ticket 04 - Get article by id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,6 +31,46 @@ describe("/api/topics", () => {
     });
   });
 });
+describe("/api/articles/:article_id", () => {
+  describe("GET", () => {
+    test("200: responds with an article object", () => {
+      return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then(({ body }) => {
+          const { article } = body;
+          expect(article).toMatchObject({
+            article_id: expect.any(Number),
+            title: expect.any(String),
+            author: expect.any(String),
+            body: expect.any(String),
+            topic: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+          });
+        });
+    });
+    test("404: responds with Not Found when article id does not exist", () => {
+      return request(app)
+        .get("/api/articles/9999")
+        .expect(404)
+        .then(({ body }) => {
+          const { msg } = body;
+          expect(msg).toBe("Not Found - Article does not exist!");
+        });
+    });
+    test("400: responds with Bad Request when article id is not valid", () => {
+      return request(app)
+        .get("/api/articles/not-an-id")
+        .expect(400)
+        .then(({ body }) => {
+          const { msg } = body;
+          expect(msg).toBe("Invalid article id!");
+        });
+    });
+  });
+});
 describe("Handle invalid endpoints", () => {
   test("responds with 404 Not Found when provided an invalid endpoint", () => {
     return request(app)
--- controllers/errors.controllers.js
+++ controllers/errors.controllers.js
@@ -1,3 +1,7 @@
-const errorHandler = (err, req, res, next) => {};
+const errorHandler = (err, req, res, next) => {
+  if (err.status === 404 || err.status === 400) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+};
 
 module.exports = { errorHandler };
