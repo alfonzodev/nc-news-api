80e1ab346bc99ee0f1c159f4d183b94b3629c57e
Jose Alfonzo <alfonzo.dev@gmail.com>
2023-03-28 13:51:06 +0100

    Solve ticket 06 - Get comments by article id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,9 +4,6 @@ const data = require("../db/data/test-data/index.js");
 const db = require("../db/connection.js");
 const seed = require("../db/seeds/seed");
 
-// Utils
-const { convertTimestampToDate } = require("../db/seeds/utils.js");
-
 beforeEach(() => {
   return seed(data);
 });
@@ -34,6 +31,40 @@ describe("/api/topics", () => {
     });
   });
 });
+
+describe("/api/articles", () => {
+  describe("GET", () => {
+    test("200: responds with an array of articles containing all rows plus the comment_count ordered by date in descending order", () => {
+      return request(app)
+        .get("/api/articles")
+        .expect(200)
+        .then(({ body }) => {
+          const { articles } = body;
+          expect(articles.length).toBe(12);
+          // Checking order by date
+          for (let i = 0; i < articles.length - 1; i++) {
+            currentDate = Date.parse(articles[i].created_at);
+            nextDate = Date.parse(articles[i + 1].created_at);
+            expect(currentDate > nextDate).toBe(true);
+          }
+          // Checking object properties
+          articles.forEach((article) => {
+            expect(article).toMatchObject({
+              author: expect.any(String),
+              title: expect.any(String),
+              article_id: expect.any(Number),
+              topic: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              article_img_url: expect.any(String),
+              comment_count: expect.any(String),
+            });
+          });
+        });
+    });
+  });
+});
+
 describe("/api/articles/:article_id", () => {
   describe("GET", () => {
     test("200: responds with an article object", () => {
@@ -66,7 +97,7 @@ describe("/api/articles/:article_id", () => {
     });
     test("400: responds with Bad Request when article id is not valid", () => {
       return request(app)
-        .get("/api/articles/not-an-id")
+        .get("/api/articles/not-a-num")
         .expect(400)
         .then(({ body }) => {
           const { msg } = body;
@@ -75,34 +106,51 @@ describe("/api/articles/:article_id", () => {
     });
   });
 });
-describe("/api/articles", () => {
+
+describe("/api/articles/:article_id/comments", () => {
   describe("GET", () => {
-    test("200: responds with an array of articles containing all rows plus the comment_count ordered by date in descending order", () => {
+    test("200: responds with an array of comments for the given article_id ordered by creation date in descending order", () => {
       return request(app)
-        .get("/api/articles")
+        .get("/api/articles/1/comments")
         .expect(200)
         .then(({ body }) => {
-          const { articles } = body;
-          expect(articles.length).toBe(12);
-          // Checking order by date
-          for (let i = 0; i < articles.length - 1; i++) {
-            currentDate = Date.parse(articles[i].created_at);
-            nextDate = Date.parse(articles[i + 1].created_at);
+          const { comments } = body;
+          expect(comments.length).toBe(11);
+          // Checking order
+          for (let i = 0; i < comments.length - 1; i++) {
+            currentDate = Date.parse(comments[i].created_at);
+            nextDate = Date.parse(comments[i + 1].created_at);
             expect(currentDate > nextDate).toBe(true);
           }
           // Checking object properties
-          articles.forEach((article) => {
-            expect(article).toMatchObject({
-              author: expect.any(String),
-              title: expect.any(String),
-              article_id: expect.any(Number),
-              topic: expect.any(String),
-              created_at: expect.any(String),
+          comments.forEach((comment) => {
+            expect(comment).toMatchObject({
+              comment_id: expect.any(Number),
               votes: expect.any(Number),
-              article_img_url: expect.any(String),
-              comment_count: expect.any(String),
+              created_at: expect.any(String),
+              author: expect.any(String),
+              body: expect.any(String),
+              article_id: expect.any(Number),
             });
           });
+        });
+    });
+    test("404: responds with Not Found if article_id does not exist", () => {
+      return request(app)
+        .get("/api/articles/300/comments")
+        .expect(404)
+        .then(({ body }) => {
+          const { msg } = body;
+          expect(msg).toBe("Not Found: article_id does not exist.");
+        });
+    });
+    test("400: responds with Bad Request if article id is invalid", () => {
+      return request(app)
+        .get("/api/articles/not-a-num/comments")
+        .expect(400)
+        .then(({ body }) => {
+          const { msg } = body;
+          expect(msg).toBe("Invalid article id!");
         });
     });
   });
