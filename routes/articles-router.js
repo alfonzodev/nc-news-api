33f44cf1fa0e44c11b8c6845213e3a53731421be
Jose Alfonzo <alfonzo.dev@gmail.com>
2023-04-18 02:23:32 +0100

    protect routes with JWT verification

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -3,7 +3,10 @@ const articlesRouter = require("express").Router();
 const {getArticles, postArticle, getArticleById,  patchArticleVotes} = require('../controllers/articles.controllers.js')
 const {getCommentsByArticleId, postComment} = require('../controllers/comments.controllers.js')
 
-articlesRouter.route("/").get(getArticles).post(postArticle);
+const {protect} = require('../middleware/auth.middleware.js');
+
+
+articlesRouter.route("/").get(getArticles).post(protect, postArticle);
 articlesRouter.route("/:article_id").get(getArticleById).patch(patchArticleVotes);
 articlesRouter.route("/:article_id/comments").get(getCommentsByArticleId).post(postComment);
 
