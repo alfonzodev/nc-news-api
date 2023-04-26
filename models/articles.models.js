fc6c38d11f8c07a1331ba54232ddbab02b09b2f9
Jose Alfonzo <alfonzo.dev@gmail.com>
2023-04-26 18:05:35 +0100

    add article body to fetchArticles select query

--- models/articles.models.js
+++ models/articles.models.js
@@ -33,7 +33,7 @@ const fetchArticles = (sort_by, order, topic, limit, p) => {
     "comment_count",
   ];
   let selectQueryStr = `
-    SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comments.comment_id) AS comment_count 
+    SELECT articles.author, title, articles.article_id, articles.body, topic, articles.created_at, articles.votes, article_img_url, COUNT(comments.comment_id) AS comment_count 
     FROM articles 
     LEFT JOIN comments ON articles.article_id = comments.article_id 
   `;
